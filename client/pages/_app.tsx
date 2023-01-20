7384d63a38c7bb5267ace7d83508c29e93ae6bec
nabin <nabin043@gmail.com>
2023-01-21 00:42:52 +0545

    apolo-connect

--- client/pages/_app.tsx
+++ client/pages/_app.tsx
@@ -1,6 +1,16 @@
 import '@/styles/globals.css'
+import { ApolloClient, ApolloProvider, InMemoryCache } from '@apollo/client';
 import type { AppProps } from 'next/app'
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+
+  const client = new ApolloClient({
+    uri: 'http://localhost:4000/graphql',
+    cache: new InMemoryCache(),
+  });
+
+  return (
+    <ApolloProvider client={client}>
+      <Component {...pageProps} />
+    </ApolloProvider>)
 }
--- client/pages/index.tsx
+++ client/pages/index.tsx
@@ -1,8 +1,28 @@
+import { gql, useQuery } from '@apollo/client';
 import React from 'react'
 
+
+const GetBooks = gql`{
+  books{
+    name
+  }
+}`;
+
 const index = () => {
+  const { loading, error, data } = useQuery(GetBooks);
+
+
+  if (loading) return <p>Loading...</p>;
+  if (error) return <p>Error : {error.message}</p>;
+
   return (
-    <div>index</div>
+    <div>
+      {data?.books?.map((itam: any) => (
+        <div>
+          <h1>{itam.name}</h1>
+        </div>
+      ))}
+    </div>
   )
 }
 
